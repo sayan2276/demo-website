a7d9542f01dcccf0de7082e73ef709e2056ebfea
sayan mukherjee <sayanmukherjee2276@gmail.com>
2025-03-15 13:16:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("changeColorBtn").addEventListener("click", function () {
+    document.body.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+});
